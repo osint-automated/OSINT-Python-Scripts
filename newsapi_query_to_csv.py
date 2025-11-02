45e171fbce2256b77168b3ff718de819220a7256
osint-automated <brandonknowles4482@gmail.com>
2025-11-02 12:20:43 -0800

    update to script

--- newsapi_query_to_csv.py
+++ newsapi_query_to_csv.py
@@ -13,7 +13,7 @@ def news_search(query):
     all_articles = newsapi.get_everything(
         q=query,
         domains=domains,
-        from_param=(datetime.now() - timedelta(days=7)).strftime('%Y-%m-%d'), #change date range as needed 30 => X days as long as X <= 30
+        from_param=(datetime.now() - timedelta(days=7)).strftime('%Y-%m-%d'), #change number of days as needed as long as X <= 30
         language='en',
     )
     results = all_articles.get('articles', [])
